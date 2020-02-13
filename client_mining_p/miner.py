129cc3ffc0e1833ddabafc71793031943a5c7c6f
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-02-13 11:58:16 -0500

    Miner works now thanks to a tutorial, lots of debugging, and help via Zoom.

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,34 +1,24 @@
 import hashlib
 import requests
-
 import sys
 import json
+import time
 
+def proof_of_work(last_block):
+        last_proof = json.dumps(last_block, sort_keys=True)
+        # Assign 0 to proof so we have a variable to increment
+        proof = 0
+        while valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
 
-def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
-
-
-def valid_proof(block_string, proof):
-    """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
-    leading zeroes?  Return true if the proof is valid
-    :param block_string: <string> The stringified block to use to
-    check in combination with `proof`
-    :param proof: <int?> The value that when combined with the
-    stringified previous block results in a hash that has the
-    correct number of leading zeroes.
-    :return: True if the resulting hash is a valid proof, False otherwise
-    """
-    pass
-
+def valid_proof(last_proof, proof):
+        # Encode the two proof arguments into bytes and assign to guess
+        guess = f"{last_proof}{proof}".encode()
+        # Use the sha256 library on (guess) , run .hexdigest method, and assign to guess_hash
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        # Return a guess_hash with six leading zeroes for exacalated difficulty
+        return guess_hash[:6] == "000000"
 
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
@@ -43,9 +33,14 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    # Initialized a coins variable at 0 so people get paid for their proofs
+    coins = 0
+
     # Run forever until interrupted
     while True:
+        # Gets the last proof stored on our server with /last_block
         r = requests.get(url=node + "/last_block")
+        # breakpoint()
         # Handle non-json response
         try:
             data = r.json()
@@ -56,15 +51,28 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
-
+        start_time = time.time()
+        new_proof = proof_of_work(data['last_block'])
+        end_time = time.time()
+        print(f'Time to mine coin: {end_time - start_time} seconds')
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
+        
+        try: 
+            data = r.json()
+        except ValueError:
+            print("Error: Non-json response")
+            print("Response returned:")
+            print(r)
+            break
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get('message') == 'New Block Forged':
+            coins += 1
+            print(f"Number of coins mined: {coins}")
+        else:
+            print(data.get('message'))
